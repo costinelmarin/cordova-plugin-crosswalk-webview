b84251d5f1820c5961927ddcfcbf41a6b247cdd9
costinelmarin <costinelmarin@gmail.com>
2020-04-07 15:26:39 -0400

    updated context.requireCordovaModule to require

--- hooks/after_plugin_install/000-shared_mode_special.js
+++ hooks/after_plugin_install/000-shared_mode_special.js
@@ -1,22 +1,20 @@
 #!/usr/bin/env node
 
-module.exports = function(context) {
+module.exports = function (context) {
+  /** @external */
+  var deferral = require("q").defer(),
+    UpdateConfig = require("./../update_config.js"),
+    updateConfig = new UpdateConfig(context);
 
-    /** @external */
-    var deferral = context.requireCordovaModule('q').defer(),
-        UpdateConfig = require('./../update_config.js'),
-        updateConfig = new UpdateConfig(context);
+  /** Main method */
+  var main = function () {
+    // Add xwalk preference to config.xml
+    updateConfig.addPreferences();
 
-    /** Main method */
-    var main = function() {
-        // Add xwalk preference to config.xml
-        updateConfig.addPreferences();
+    deferral.resolve();
+  };
 
-        deferral.resolve();
-    };
+  main();
 
-    main();
-
-    return deferral.promise;
-
+  return deferral.promise;
 };
